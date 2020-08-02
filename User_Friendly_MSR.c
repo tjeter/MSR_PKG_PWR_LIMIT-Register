25529f6b5a29effd0789cdfbc683e26b184df64c
Tre' Jeter <33637310+tjeter@users.noreply.github.com>
2020-08-01 23:03:08 -0600

    Update User_Friendly_MSR.c

--- User_Friendly_MSR.c
+++ User_Friendly_MSR.c
@@ -1,5 +1,5 @@
 /***********************************************************************************
-Programmer: Tre' Jeter, Computation Scholar at LLNL
+Programmer: Tre' Jeter
 Date: May 29, 2019
 Purpose: Find the values in the open fields of Intel's MSR_PKG_POWER_LIMIT Register
          in terms of power in watts and in terms of time in seconds
